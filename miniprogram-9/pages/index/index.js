69d9d53a9650c9f69e840d2d80b790e53843d61d
140825866 <91413142+140825866@users.noreply.github.com>
2021-09-26 18:08:05 +0800

    Add files via upload

---
+++ miniprogram-9/pages/index/index.js
@@ -0,0 +1,20 @@
+// index.js
+// 获取应用实例
+var p,i,x,sum;
+Page({
+  principal:function(e){
+    p=parseInt(e.detail.value);
+  },
+  interestrate:function(e){
+    i=parseInt(e.detail.value);
+  },
+  calc:function(){
+    x = p;
+    for(var n=1;n<=5;n++){
+      x=x*i/100+x
+    }
+    this.setData({
+      sum:x
+    })
+  }
+})
